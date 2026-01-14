75df77d62569d153155b3de47d79c88aee062245
Cyril ALFARO <cyril@dbyzero.com>
2026-01-14 19:40:13 +0100

    fix: remove weapon and spell, remove race item type

--- src/module/models/item-feat.ts
+++ src/module/models/item-feat.ts
@@ -304,7 +304,6 @@ export class FeatDataModel extends foundry.abstract.TypeDataModel<any, Item> {
           "cyberware": "SRA2.FEATS.FEAT_TYPE.CYBERWARE",
           "cyberdeck": "SRA2.FEATS.FEAT_TYPE.CYBERDECK",
           "vehicle": "SRA2.FEATS.FEAT_TYPE.VEHICLE",
-          "weapons-spells": "SRA2.FEATS.FEAT_TYPE.WEAPONS_SPELLS",
           "weapon": "SRA2.FEATS.FEAT_TYPE.WEAPON",
           "spell": "SRA2.FEATS.FEAT_TYPE.SPELL",
           "connaissance": "SRA2.FEATS.FEAT_TYPE.KNOWLEDGE",
